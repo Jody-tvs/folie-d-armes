bf2bf4c359db9bda75e19917d7de8bf39907d0f6
Carole Tavares <jtavares.mcd@gmail.com>
2024-09-20 13:41:23 +0200

    Ajout des modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
@@ -7,8 +8,11 @@ import Armes from './pages/Armes'
 import Accessoires from './pages/Accessoires'
 import Munitions from './pages/Munitions'
 
+
+
 const App = () => (
   <Router>
+    <layout>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
@@ -17,6 +21,7 @@ const App = () => (
       <Route path="/categories/accessoires" element={<Accessoires />} />
       <Route path="/categories/munitions" element={<Munitions />} />
     </Routes>
+    </layout>
   </Router>
 )
 
